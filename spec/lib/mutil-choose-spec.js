6958e57982c68d5917a1ae794787c10689adfe4d
purple <purpletianjing@163.com>
2015-08-07 01:31:16 +0800

    add mutil-choose.js

--- spec/lib/mutil-choose-spec.js
+++ spec/lib/mutil-choose-spec.js
@@ -1,4 +1,8 @@
 describe("MutilChoose test",function() {
   var mutilChoose = new MutilChoose();
-
+  it('get score',function() {
+    expect(mutilChoose.getScore(['A','B'],['A','B'])).toBe(3);
+    expect(mutilChoose.getScore(['A'],['A','B'])).toBe(2);
+    expect(mutilChoose.getScore(['C'],['A','B'])).toBe(0);
+  });
 });
